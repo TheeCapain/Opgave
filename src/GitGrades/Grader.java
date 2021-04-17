161c250fd829ec60a1d0a49d801b12caa07e2711
Kamille Nikolajsen <kamillenikolajsen@Kamilles-MacBook-Pro-3.local>
2021-04-17 10:33:18 +0200

    tiels added in Grader class

--- src/GitGrades/Grader.java
+++ src/GitGrades/Grader.java
@@ -3,11 +3,12 @@ package GitGrades;
 import java.util.Random;
 
 public class Grader {
+    //ATTRIBUTES
     Random rand = new Random();
     private final int[] allGrades = new int[]{-3, 0, 2, 4, 7, 10, 12};
     private final String[] classes = {"Spanish: ", "Math: ", "English: ", "Science: ", "Conspiracy theories 101: ", "Biology: ", "Programming: "};
 
-
+    //METHODS
     public void registerGrades(Student student) {
         for (int i = 0; i < classes.length; i++) {
             int currentGrade = allGrades[rand.nextInt(allGrades.length)];
