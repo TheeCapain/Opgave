c780aef95d34348e998ec4508895aff99a57e002
August <AugustHauerslev@outlook.com>
2021-04-17 10:34:11 +0200

    Made changes

--- src/GitGrades/Main.java
+++ src/GitGrades/Main.java
@@ -7,11 +7,9 @@ public class Main {
 
     public static void run() {
         Student student = new Student("John");
-        student.getGrades();
-
         Student student1 = new Student("Kamille");
-
         Grader grader = new Grader();
+
         System.out.println(student.getName());
         for(int i = 0; i < student.getGrades().length;i++){
             System.out.println(grader.getClasses()[i]+student.getGrades()[i]);
