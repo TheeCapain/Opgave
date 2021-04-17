7f74c3ed3f98a1dc0661d33c1a0ee9357de7dd67
August <AugustHauerslev@outlook.com>
2021-04-17 10:23:44 +0200

    ExamMethod added

--- src/GitGrades/Main.java
+++ src/GitGrades/Main.java
@@ -6,17 +6,23 @@ public class Main {
     }
 
     public static void run() {
-        Student student = new Student("John");
-        Grader grader = new Grader();
 
+
+
+
+    }
+
+    public void printExamResults(){
+        Student student = new Student("John");
         student.StudentInfo();
         student.getGrades();
 
+        Student student1 = new Student("Kamille");
+
+        Grader grader = new Grader();
         System.out.println(student.getName());
         for(int i = 0; i < student.getGrades().length;i++){
             System.out.println(grader.getClasses()[i]+student.getGrades()[i]);
         }
-        
-        Student student1 = new Student("Kamille");
     }
 }
